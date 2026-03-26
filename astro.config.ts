eee88fffa91a4de7d0de5102df6b7ca67c19a0a4
ErrorCezar <mobilesgame55@proton.me>
2026-03-26 15:50:55 +0100

    Documentation progress

--- astro.config.ts
+++ astro.config.ts
@@ -1,42 +1,44 @@
-import starlight from '@astrojs/starlight'
-import { defineConfig } from 'astro/config'
-import starlightThemeGalaxy from 'starlight-theme-galaxy'
-
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightThemeGalaxy from "starlight-theme-galaxy";
 
 export default defineConfig({
   // site: 'https://localhost:4321/starlight-theme-galaxy',
-  base: '/lightborn',
+  base: "/lightborn",
   integrations: [
     starlight({
-      title: 'LightBorn',
-      favicon: '/favicon.svg',
+      title: "LightBorn",
+      favicon: "/favicon.svg",
       // defaultLocale: 'en',
       // locales: {
       //   en: {
       //     label: 'English',
       //     lang: 'en',
-      //   },  
+      //   },
       //   fr: {
       //     label: 'French',
       //     lang: 'fr',
       //   },
       // },
       editLink: {
-        baseUrl: 'https://github.com/Error-Cezar/LightBorn/edit/docs/src/',
+        baseUrl: "https://github.com/Error-Cezar/LightBorn/edit/docs/src/",
       },
-      tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 4},
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
       plugins: [starlightThemeGalaxy()],
       sidebar: [
         {
-          label: 'Start Here',
-          items: ['getting-started', 'customization', 'components-override'],
+          label: "Start Here",
+          items: ["getting-started", "customization", "components-override"],
         },
-        { label: 'Examples', autogenerate: { directory: 'examples' } },
-        { label: 'Custom Components', autogenerate: { directory: 'components' } },
-      ],  
+        { label: "Examples", autogenerate: { directory: "examples" } },
+      ],
       social: [
-        { href: 'https://github.com/Error-Cezar/LightBorn', icon: 'github', label: 'GitHub' },
-      ],      
+        {
+          href: "https://github.com/Error-Cezar/LightBorn",
+          icon: "github",
+          label: "GitHub",
+        },
+      ],
     }),
   ],
-})
+});
