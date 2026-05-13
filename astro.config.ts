b5c6be6cb70187ac26bceab0ecc4388446e3373a
Error-Cezar <mobilesgame55@proton.me>
2026-05-13 09:37:05 +0200

    chore: astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       title: "LightBorn",
       favicon: "/favicon.svg",
       editLink: {
-        baseUrl: "https://github.com/Error-Cezar/LightBorn/edit/docs/src/",
+        baseUrl: "https://github.com/LightBorn-lua/LightBorn/edit/docs/src/",
       },
       tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
       plugins: [starlightThemeGalaxy()],
@@ -22,7 +22,7 @@ export default defineConfig({
       ],
       social: [
         {
-          href: "https://github.com/Error-Cezar/LightBorn",
+          href: "https://github.com/LightBorn-lua/LightBorn",
           icon: "github",
           label: "GitHub",
         },
